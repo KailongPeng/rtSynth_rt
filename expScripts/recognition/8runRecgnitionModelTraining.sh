ced34c1bc4aa86b8ba974c929d6a8350e46ce8b2
KailongPeng <kailongpeng001@gmail.com>
2021-06-17 14:04:51 -0400

    conda

--- expScripts/recognition/8runRecgnitionModelTraining.sh
+++ expScripts/recognition/8runRecgnitionModelTraining.sh
@@ -6,12 +6,8 @@
 #SBATCH --time=6:00:00 #20:00:00
 #SBATCH --mem 4GB
 #SBATCH -n 1
-module load FSL
-# module load miniconda
-# . /gpfs/milgram/apps/hpc.rhel7/software/Python/Anaconda3/etc/profile.d/conda.sh
-# source activate /gpfs/milgram/project/turk-browne/users/kp578/CONDA/rtcloud
-source ~/.bashrc
-cd_rt
+
+cd /gpfs/milgram/project/turk-browne/projects/rt-cloud ; module load AFNI ; module load FSL ; source /gpfs/milgram/apps/hpc.rhel7/software/FSL/6.0.3-centos7_64/etc/fslconf/fsl.sh ; module load dcm2niix ; . /gpfs/milgram/apps/hpc.rhel7/software/Python/Anaconda3/etc/profile.d/conda.sh ; conda activate /gpfs/milgram/project/turk-browne/kp578/conda_envs/rtSynth_rt
 
 toml=$1 # sub002.ses5.toml
 scan_asTemplate=$2 # 1
