814b185d7bb229808a30925d00e702976760d378
KailongPeng <kailongpeng001@gmail.com>
2021-06-17 13:53:51 -0400

    new folder structure

--- expScripts/recognition/8runRecgnitionModelTraining.sh
+++ expScripts/recognition/8runRecgnitionModelTraining.sh
@@ -8,9 +8,10 @@
 #SBATCH -n 1
 module load FSL
 # module load miniconda
-. /gpfs/milgram/apps/hpc.rhel7/software/Python/Anaconda3/etc/profile.d/conda.sh
-source activate /gpfs/milgram/project/turk-browne/users/kp578/CONDA/rtcloud
-
+# . /gpfs/milgram/apps/hpc.rhel7/software/Python/Anaconda3/etc/profile.d/conda.sh
+# source activate /gpfs/milgram/project/turk-browne/users/kp578/CONDA/rtcloud
+source ~/.bashrc
+cd_rt
 
 toml=$1 # sub002.ses5.toml
 scan_asTemplate=$2 # 1
@@ -19,9 +20,9 @@ tmp_folder=$4
 skipGreedy=$5 # 1 is skipping 0 is not skipping
 if [ "$forceGreedy" == "1" ]; then
     echo "forceGreedy"
-    python -u /gpfs/milgram/project/turk-browne/projects/rtSynth_rt/expScripts/recognition/8runRecgnitionModelTraining.py -c $toml --scan_asTemplate $scan_asTemplate --skipPre --forceGreedy --tmp_folder $tmp_folder
+    python -u projects/rtSynth_rt/expScripts/recognition/8runRecgnitionModelTraining.py -c $toml --scan_asTemplate $scan_asTemplate --skipPre --forceGreedy --tmp_folder $tmp_folder
 else
     echo "not forceGreedy"
-    python -u /gpfs/milgram/project/turk-browne/projects/rtSynth_rt/expScripts/recognition/8runRecgnitionModelTraining.py -c $toml --scan_asTemplate $scan_asTemplate --skipGreedy ${skipGreedy}
+    python -u projects/rtSynth_rt/expScripts/recognition/8runRecgnitionModelTraining.py -c $toml --scan_asTemplate $scan_asTemplate --skipGreedy ${skipGreedy}
 fi
 
