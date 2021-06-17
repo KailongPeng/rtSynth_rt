9c7e9499a9b9b6903d8a538133ac0193f9c9adc0
KailongPeng <kailongpeng001@gmail.com>
2021-06-17 13:52:42 -0400

    bug

--- expScripts/bashrc.sh
+++ expScripts/bashrc.sh
@@ -14,7 +14,7 @@
     alias cd_rtt="cd /Users/kailong/Desktop/rtEnv/rt-cloud/projects/rtSynth_rt/"
 
 # Milgram
-    function pinterface { bash scripts/run-projectInterface.sh -ip 172.29.189.${1} --subjectRemote -p rtSynth_rt -c /gpfs/milgram/project/turk-browne/projects/rt-cloud/projects/rtSynth_rt/conf/${2}.${3}.toml} 
+    function pinterface { bash scripts/run-projectInterface.sh -ip 172.29.189.${1} --subjectRemote -p rtSynth_rt -c /gpfs/milgram/project/turk-browne/projects/rt-cloud/projects/rtSynth_rt/conf/${2}.${3}.toml ; } 
     # pinterface 151 sub003 ses6
 
     alias cd_rt="cd /gpfs/milgram/project/turk-browne/projects/rt-cloud ; module load AFNI ; module load FSL ; source /gpfs/milgram/apps/hpc.rhel7/software/FSL/6.0.3-centos7_64/etc/fslconf/fsl.sh ; module load dcm2niix ; . /gpfs/milgram/apps/hpc.rhel7/software/Python/Anaconda3/etc/profile.d/conda.sh ; conda activate /gpfs/milgram/project/turk-browne/kp578/conda_envs/rtSynth_rt"
