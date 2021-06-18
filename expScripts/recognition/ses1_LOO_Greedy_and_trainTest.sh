7340bbfe65b6d49d9c90c8901dffb0fbf4c4339f
KailongPeng <kailongpeng001@gmail.com>
2021-06-17 21:34:33 -0400

    new conda

--- expScripts/recognition/ses1_LOO_Greedy_and_trainTest.sh
+++ expScripts/recognition/ses1_LOO_Greedy_and_trainTest.sh
@@ -5,14 +5,11 @@
 #SBATCH --time=6:00:00 #20:00:00
 #SBATCH --mem 4GB
 #SBATCH -n 1
-module load FSL
-# module load miniconda
-. /gpfs/milgram/apps/hpc.rhel7/software/Python/Anaconda3/etc/profile.d/conda.sh
-conda activate /gpfs/milgram/project/turk-browne/users/kp578/CONDA/rtcloud
+cd /gpfs/milgram/project/turk-browne/projects/rt-cloud ; module load AFNI ; module load FSL ; source /gpfs/milgram/apps/hpc.rhel7/software/FSL/6.0.3-centos7_64/etc/fslconf/fsl.sh ; module load dcm2niix ; . /gpfs/milgram/apps/hpc.rhel7/software/Python/Anaconda3/etc/profile.d/conda.sh ; conda activate /gpfs/milgram/project/turk-browne/kp578/conda_envs/rtSynth_rt
 
 toml=$1 # sub002.ses5.toml
 LeaveOutRun=$2
 tmp_folder=$3
-echo python -u /gpfs/milgram/project/turk-browne/projects/rtSynth_rt/expScripts/recognition/ses1_LOO_Greedy_and_trainTest.py -c $toml --LeaveOutRun $LeaveOutRun --tmp_folder $tmp_folder --jobID ${SLURM_JOBID}
-python -u /gpfs/milgram/project/turk-browne/projects/rtSynth_rt/expScripts/recognition/ses1_LOO_Greedy_and_trainTest.py -c $toml --LeaveOutRun $LeaveOutRun --tmp_folder $tmp_folder --jobID ${SLURM_JOBID}
+echo python -u /gpfs/milgram/project/turk-browne/projects/rt-cloud/projects/rtSynth_rt/expScripts/recognition/ses1_LOO_Greedy_and_trainTest.py -c $toml --LeaveOutRun $LeaveOutRun --tmp_folder $tmp_folder --jobID ${SLURM_JOBID}
+python -u /gpfs/milgram/project/turk-browne/projects/rt-cloud/projects/rtSynth_rt/expScripts/recognition/ses1_LOO_Greedy_and_trainTest.py -c $toml --LeaveOutRun $LeaveOutRun --tmp_folder $tmp_folder --jobID ${SLURM_JOBID}
 
