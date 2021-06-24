8ccc1fe59516982a809c821bf2dacd0dda0661ae
KailongPeng <kailongpeng001@gmail.com>
2021-06-24 11:12:12 -0400

    2runrecognitionDataAnalysis

--- expScripts/bashrc.sh
+++ expScripts/bashrc.sh
@@ -25,6 +25,9 @@
     function chooseGeneratingCode { python projects/rtSynth_rt/expScripts/recognition/chooseGeneratingCode.py -c ${1}.${2}.toml ; } 
     # chooseGeneratingCode sub004 ses1
 
+    function 2runrecognitionDataAnalysis { python projects/rtSynth_rt/expScripts/recognition/2runrecognitionDataAnalysis.py -c  ${1}.${2}.toml  --scan_asTemplate ${3} ; } 
+    # 2runrecognitionDataAnalysis sub004 ses2 1
+
     alias cd_rt="cd /gpfs/milgram/project/turk-browne/projects/rt-cloud ; module load AFNI ; module load FSL ; source /gpfs/milgram/apps/hpc.rhel7/software/FSL/6.0.3-centos7_64/etc/fslconf/fsl.sh ; module load dcm2niix ; . /gpfs/milgram/apps/hpc.rhel7/software/Python/Anaconda3/etc/profile.d/conda.sh ; conda activate /gpfs/milgram/project/turk-browne/kp578/conda_envs/rtSynth_rt"
     alias cd_rtt="cd /gpfs/milgram/project/turk-browne/projects/rt-cloud/projects/rtSynth_rt/"
 
